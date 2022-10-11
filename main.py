f182797a4797812a7b7b33b799fa7ea890016b37
Javier Valenzuela <javier.valenzuelac@mail.udp.cl>
2022-10-11 00:32:49 -0300

    ref: use example 1 as default battle

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from kombat.battle import Battle
 from kombat.player import Player, Attack
 
 def main():
-    # battle_sequence: str = '{"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2":{"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}'
+    battle_sequence: str = '{"player1":{"movimientos":["D","DSD","S","DSD","SD"],"golpes":["K","P","","K","P"]},"player2":{"movimientos":["SA","SA","SA","ASA","SA"],"golpes":["K","","K","P","P"]}}'
     # battle_sequence: str = '{"player1":{"movimientos":["SDD", "DSD", "SA", "DSD"] ,"golpes":["K", "P", "K", "P"]},"player2":{"movimientos":["DSD", "WSAW", "ASA", "", "ASA", "SA"],"golpes":["P", "K", "K", "K", "P","k"]}}'
-    battle_sequence: str = '{"player1":{"movimientos":["DSD", "S"] ,"golpes":[ "P", ""]},"player2":{"movimientos":["", "ASA", "DA", "AAA", "", "SA"],"golpes":["P", "", "P", "K", "K", "K"]}}'
+    # battle_sequence: str = '{"player1":{"movimientos":["DSD", "S"] ,"golpes":[ "P", ""]},"player2":{"movimientos":["", "ASA", "DA", "AAA", "", "SA"],"golpes":["P", "", "P", "K", "K", "K"]}}'
     print(battle_sequence)
     battle: Battle = Battle(battle_sequence=battle_sequence)
 
